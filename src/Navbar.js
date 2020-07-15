4e6a6cdc2a3a6132255aa0264449ddd77f235ca9
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-15 16:39:15 +0500

    Navbar Done

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './Navbar.css'
 
 export default function Navbar() {
+
+    const [showNav, setShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if(window.scrollY > 100){
+                setShow(true);
+            }
+            else{
+                setShow(false);
+            }
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, [])
+
     return (
-        <div className="nav">
+        <div className={`nav ${showNav && "nav_black"}`}>
             <img 
                 className="nav_logo"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
