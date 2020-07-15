3eeab17b2ee6201fa5cd462b5bd7c7a4daaffb85
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-15 15:59:45 +0500

    Banner Done

--- src/Banner.js
+++ src/Banner.js
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react'
 // API Call
 import axios from "./axios"
 
+// Styling
+import './Banner.css'
+
 export default function Banner({ fetchUrl }) {
 
     const [movie, setMovie] = useState([]);
@@ -18,11 +21,35 @@ export default function Banner({ fetchUrl }) {
             return request;
         }
         fetchData();
-    }, [fetchUrl])
+    }, [fetchUrl]);
+
+    function truncate(str, n){
+        return (str.length > n) ? str.substr(0, n-1) + '...' : str;
+    };
 
     return (
-        <header>
-            
+        <header 
+            className="banner"
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+                backgroundPosition: 'center center',
+            }}
+        >
+            <div className="banner_contents">
+                <h1 className="banner_title">
+                    { movie?.title || movie?.name || movie?.original_name }
+                </h1>
+                <div className="banner_buttons">
+                    <button className="banner_button">Play</button>
+                    <button className="banner_button">My List</button>
+                </div>
+                <h1 className="banner_description">
+                    { truncate(String(movie?.overview), 160) }
+                </h1>
+            </div>
+
+            <div className="banner_fadeBottom" />
         </header>
     )
 }
