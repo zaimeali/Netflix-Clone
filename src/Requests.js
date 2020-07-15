7bb6438bc63df6a941fe8935808d0bc83e3a9b45
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-15 13:21:01 +0500

    Created Endpoints

---
+++ src/Requests.js
@@ -0,0 +1,16 @@
+// If it is not a component so smaller case in initial of a word.
+
+const API_KEY = "d41159700e14d44ceb8c9c5895ae92de";
+
+const requests = {
+    fetchTrending: `/trending/all/day?api_key=${API_KEY}`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
